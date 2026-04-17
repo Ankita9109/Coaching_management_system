5891ba262cda650bd677c8739823702c86daa1c2
Ankita9109 <ankitajaiswal.2910@gmail.com>
2026-04-17 18:23:34 +0530

    Updated build file

--- build.sh
+++ build.sh
@@ -1,5 +1,18 @@
 #!/usr/bin/env bash
+echo "---Updating pip---"
+pip install --upgrade pip
+
+echo "---Installing requirements---"
 pip install -r requirements.txt
+
+echo "---Colleting statics files---"
 python manage.py collectstatic --noinput
+
+echo "---Migrating database---"
 python manage.py migrate
+
+echo "---Loading user fixtures---"
 python3 manage.py loaddata users
+
+# echo "---Starting server---"
+# python manage.py runserver 8001
